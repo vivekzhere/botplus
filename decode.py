4691c42d9eddcc2e2c3cf37d2cc0470c8258f9a3
Vivek Anand T Kallampally <vivekzhere@gmail.com>
2012-10-21 14:37:41 +0530

    Debugging stage

--- decode.py
+++ decode.py
@@ -9,125 +9,131 @@ from PIL import Image
 from copy import deepcopy as copy
 from dct import *
 
-def decode(filename,l):
-	im=Image.open(filename)
-	img_src = scipy.misc.fromimage(im,flatten=0)
+#def decode(filename,l):
+filename = "newimage.jpg"
+l=27
+im=Image.open(filename)
+img_src = scipy.misc.fromimage(im,flatten=0)
 
-	if im.mode != 'L':
-		img_component = img_src[...,1]
-	else:
-		img_component = img_src
-	if(img_component.shape[0] < 256 or img_component.shape[1] < 256):
-		return -1
-	else:
-		#img = img_component[0:256,0:256]
-		img = img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ]
+if im.mode != 'L':
+	img_component = img_src[...,1]
+else:
+	img_component = img_src
+if(img_component.shape[0] < 256 or img_component.shape[1] < 256):
+	#return -1
+	print "Error"
+else:
+	#img = img_component[0:256,0:256]
+	img = img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ]
 
-	# Set a seed for random number generator
-	random.seed("agRJJHoiefxn8328D24kg")
+# Set a seed for random number generator
+random.seed("agRJJHoiefxn8328D24kg")
 
-	q = 5        #Rate of enccoding for RA Codes
+q = 5        #Rate of enccoding for RA Codes
 #	l = 29        # Length of encoded string
 
-	#Finding the same random permutation
-	permvector = range(q*l*8)
-	random.shuffle(permvector)
+#Finding the same random permutation
+permvector = range(q*l*8)
+random.shuffle(permvector)
 
-	#Finding inverse permuation vector
-	ipermvector = copy(permvector)
-	for i in range(q*l*8):
-		ipermvector[permvector[i]] = i
-		
-	M = img.shape[0]
-	N = img.shape[1]
-	B = 10			#Block Size 
-	Mb =  np.uint8(np.floor(M/B))
-	Nb =  np.uint8(np.floor(N/B))
-	Qf = 50;
+#Finding inverse permuation vector
+ipermvector = copy(permvector)
+for i in range(q*l*8):
+	ipermvector[permvector[i]] = i
+	
+M = img.shape[0]
+N = img.shape[1]
+B = 10			#Block Size 
+Mb =  np.uint8(np.floor(M/B))
+Nb =  np.uint8(np.floor(N/B))
+Qf = 50;
 
-	QM = [[16,  11,  10,  16,  24,   40,   51,   61],   
-	[12,  12,  14,  19,  26,   58,   60,   55],   
-	[14,  13,  16,  24,  40,   57,   69,   56],   
-	[14,  17,  22,  29,  51,   87,   80,   62],   
-	[18,  22,  37,  56,  68,   109,  103,  77],   
-	[24,  35,  55,  64,  81,   104,  113,  92],   
-	[49,  64,  78,  87,  103,  121,  120,  101], 
-	[72,  92,  95,  98,  112,  100,  103,  99]]
+QM = [[16,  11,  10,  16,  24,   40,   51,   61],   
+[12,  12,  14,  19,  26,   58,   60,   55],   
+[14,  13,  16,  24,  40,   57,   69,   56],   
+[14,  17,  22,  29,  51,   87,   80,   62],   
+[18,  22,  37,  56,  68,   109,  103,  77],   
+[24,  35,  55,  64,  81,   104,  113,  92],   
+[49,  64,  78,  87,  103,  121,  120,  101], 
+[72,  92,  95,  98,  112,  100,  103,  99]]
 
-	if Qf < 50:	
-		S = 5000/Qf
-	else:
-		S = 200 - 2*Qf
+if Qf < 50:	
+	S = 5000/Qf
+else:
+	S = 200 - 2*Qf
 
-	newQM = QM
-	if S !=	0:
-		newQM = np.divide(np.multiply(QM,S),100)
-	######
-	bitstream=[]
-	bitCount = 0;
+newQM = QM
+if S !=	0:
+	newQM = np.divide(np.multiply(QM,S),100)
+######
+bitstream=[]
+bitCount = 0;
 
-	for I in range(Mb):
+for I in range(Mb):
+	if(bitCount >= q*l*8):  
+		break  
+	for J in range(Nb):
 		if(bitCount >= q*l*8):  
 			break  
-		for J in range(Nb):
-			if(bitCount >= q*l*8):  
-				break  
-			Sx = random.randrange(0,B-7)
-			Sy = random.randrange(0,B-7)
-		
-			randBlock = copy(img[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)])
-			randBlock = np.add(randBlock,-128)
-			randBlockDCT = dct2(randBlock)
-			randBlockQ = np.divide(randBlockDCT,newQM)
-			##Decoding
-			rowstep = -1		
-			colstep = 1
-			i = 0
-			j = 0
-			#Travelling zigzag
-			for n in range(1,20):      
-				i = i + rowstep
-				j = j + colstep
-				if (i < 0):
-					i = i + 1	
-					colstep = colstep * -1
-					rowstep = rowstep * -1
-				if ( j < 0 ):
-					j = j + 1
-					colstep = colstep * -1
-					rowstep = rowstep * -1
-				absval = abs(round(randBlockQ[i][j]))
-				if (absval > 0):
-					if (absval%2 == 0):
-						#print randBlockQ[i][j] , 0
-						bitstream.append(0)
-						bitCount = bitCount + 1
-					else:
-						#print randBlockQ[i][j] , 1
-						bitstream.append(1)
-						bitCount = bitCount + 1
-					
-	if bitstream.__len__() >= q*l*8:				
-		bitstream = bitstream[:q*l*8]
-		drepbitstream = copy(bitstream)
-		for i in range(bitstream.__len__()):
-			drepbitstream[i] = bitstream[ipermvector[i]]
+		Sx = random.randrange(0,B-7)
+		Sy = random.randrange(0,B-7)
+	
+		randBlock = copy(img[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)])
+		randBlock = np.add(randBlock,-128)
+		randBlockDCT = dct2(randBlock)
+		randBlockQ = np.divide(randBlockDCT,newQM)
+		##Decoding
+		rowstep = -1		
+		colstep = 1
+		i = 0
+		j = 0
+		#Travelling zigzag
+		for n in range(1,20):      
+			i = i + rowstep
+			j = j + colstep
+			if (i < 0):
+				i = i + 1	
+				colstep = colstep * -1
+				rowstep = rowstep * -1
+			if ( j < 0 ):
+				j = j + 1
+				colstep = colstep * -1
+				rowstep = rowstep * -1
+			absval = abs(round(randBlockQ[i][j]))
+			if (absval > 0):
+				if (absval%2 == 0):
+					#print randBlockQ[i][j] , 0
+					bitstream.append(0)
+					bitCount = bitCount + 1
+				else:
+					#print randBlockQ[i][j] , 1
+					bitstream.append(1)
+					bitCount = bitCount + 1
+				
+if bitstream.__len__() >= q*l*8:				
+	bitstream = bitstream[:q*l*8]
+	drepbitstream = copy(bitstream)
+	for i in range(bitstream.__len__()):
+		drepbitstream[i] = bitstream[ipermvector[i]]
 
-		#Finding the correct bit per block 
-		obitstream = []
-		for i in range(l*8):
-			if(drepbitstream[i*q:(i+1)*q].count(1) >= 3):
-				obitstream.append('1')
-			elif(drepbitstream[i*q:(i+1)*q].count(0) >= 3):
-				obitstream.append('0')
-			else:
-				return -2
+	#Finding the correct bit per block 
+	obitstream = []
+	for i in range(l*8):
+		if(drepbitstream[i*q:(i+1)*q].count(1) >= 3):
+			obitstream.append('1')
+		elif(drepbitstream[i*q:(i+1)*q].count(0) >= 3):
+			obitstream.append('0')
+		else:
+			print "Error2"
+			#return -2
 
-		decodestream="".join(obitstream)
-		decodestream='0b'+decodestream
-		n = int(decodestream, 2)
-		decodestr=unhexlify('%x' % n)
-		return decodestr
-		#return bitstream
-	else:
-		return -3
+	decodestream="".join(obitstream)
+	decodestream='0b'+decodestream
+	n = int(decodestream, 2)
+	decodestr=unhexlify('%x' % n)
+	print decodestr
+	#return decodestr
+	#return bitstream
+else:
+	print "Error 3"
+	#return -3
--- encode.py
+++ encode.py
@@ -8,154 +8,157 @@ from PIL import Image
 from copy import deepcopy as copy
 from dct import *
 
-def encode(filename,message):
-	im = Image.open(filename)
-	img_src = scipy.misc.fromimage(im,flatten=0)
+#def encode(filename,message):
+filename = "image.jpg"
+message = "10abcd1234*#vivekzhere#G"	
+im = Image.open(filename)
+img_src = scipy.misc.fromimage(im,flatten=0)
 
-	if im.mode == 'RGB':
-		img_component = img_src[...,1]
-	elif im.mode == 'L':
-		img_component = img_src
-	else:
-		print "Error"
-	if(img_component.shape[0] < 256 or img_component.shape[1] < 256):
-		print "Error"
-	else:
-		#img = img_component[0:256,0:256]
-		img = img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ]
+if im.mode == 'RGB':
+	img_component = img_src[...,1]
+elif im.mode == 'L':
+	img_component = img_src
+else:
+	print "Error"
+if(img_component.shape[0] < 256 or img_component.shape[1] < 256):
+	print "Error"
+else:
+	#img = img_component[0:256,0:256]
+	img = img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ]
+
+#Set a seed for random number generator
+random.seed("agRJJHoiefxn8328D24kg")
+
+asciilist = [ord(c) for c in message]
+binlist = ['{0:08b}'.format(i) for i in asciilist]
+binstr = "".join(binlist)
+q = 5
+repbitstream = [bit*q for bit in binstr]
+repbitstring = "".join(repbitstream)
+repbitstream = [b for b in repbitstring]
+
+
+permvector = range(repbitstream.__len__())
+random.shuffle(permvector)
+permbitstream = copy(repbitstream)
+for i in range(repbitstream.__len__()):
+	permbitstream[i] = repbitstream[permvector[i]]
+
+accbitstream = permbitstream
+bitCount=0	# no of bit encoded
 	
-	#Set a seed for random number generator
-	random.seed("agRJJHoiefxn8328D24kg")
+	
+M = img.shape[0]
+N = img.shape[1]
+B = 10			#Block Size 
+Mb =  np.uint8(np.floor(M/B))
+Nb =  np.uint8(np.floor(N/B))
+Qf = 50;
 
-	asciilist = [ord(c) for c in message]
-	binlist = ['{0:08b}'.format(i) for i in asciilist]
-	binstr = "".join(binlist)
-	q = 5
-	repbitstream = [bit*q for bit in binstr]
-	repbitstring = "".join(repbitstream)
-	repbitstream = [b for b in repbitstring]
+QM = [[16,  11,  10,  16,  24,   40,   51,   61],   
+[12,  12,  14,  19,  26,   58,   60,   55],   
+[14,  13,  16,  24,  40,   57,   69,   56],   
+[14,  17,  22,  29,  51,   87,   80,   62],   
+[18,  22,  37,  56,  68,   109,  103,  77],   
+[24,  35,  55,  64,  81,   104,  113,  92],   
+[49,  64,  78,  87,  103,  121,  120,  101], 
+[72,  92,  95,  98,  112,  100,  103,  99]]
 
+if Qf < 50:	
+	S = 5000/Qf
+else:
+	S = 200 - 2*Qf
 
-	permvector = range(repbitstream.__len__())
-	random.shuffle(permvector)
-	permbitstream = copy(repbitstream)
-	for i in range(repbitstream.__len__()):
-		permbitstream[i] = repbitstream[permvector[i]]
+newQM = QM
+if S !=	0:
+	newQM = np.divide(np.multiply(QM,S),100)
 
-	accbitstream = permbitstream
-	bitCount=0	# no of bit encoded
-		
-		
-	M = img.shape[0]
-	N = img.shape[1]
-	B = 10			#Block Size 
-	Mb =  np.uint8(np.floor(M/B))
-	Nb =  np.uint8(np.floor(N/B))
-	Qf = 50;
-
-	QM = [[16,  11,  10,  16,  24,   40,   51,   61],   
-	[12,  12,  14,  19,  26,   58,   60,   55],   
-	[14,  13,  16,  24,  40,   57,   69,   56],   
-	[14,  17,  22,  29,  51,   87,   80,   62],   
-	[18,  22,  37,  56,  68,   109,  103,  77],   
-	[24,  35,  55,  64,  81,   104,  113,  92],   
-	[49,  64,  78,  87,  103,  121,  120,  101], 
-	[72,  92,  95,  98,  112,  100,  103,  99]]
-
-	if Qf < 50:	
-		S = 5000/Qf
-	else:
-		S = 200 - 2*Qf
-
-	newQM = QM
-	if S !=	0:
-		newQM = np.divide(np.multiply(QM,S),100)
-
-	newimg = copy(img)
+newimg = copy(img)
 
 
-	for I in range(Mb):
+for I in range(Mb):
+	if(bitCount >= accbitstream.__len__()):  
+		break   
+	for J in range(Nb):
 		if(bitCount >= accbitstream.__len__()):  
-			break   
-		for J in range(Nb):
-			if(bitCount >= accbitstream.__len__()):  
-				break  
-			Sx = random.randrange(0,B-7)
-			Sy = random.randrange(0,B-7)	
+			break  
+		Sx = random.randrange(0,B-7)
+		Sy = random.randrange(0,B-7)	
 
-			randBlock = copy(img[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)])
-			randBlock = np.add(randBlock,-128)
-			randBlockDCT = dct2(randBlock)
-			randBlockQ = np.divide(randBlockDCT,newQM)
-		
-			##Embedding
-			zigzag = np.zeros(20)
-			zigzag[0] = randBlockQ[0][0]
-			rowstep = -1		
-			colstep = 1
-			i = 0
-			j = 0
-			#Travelling zigzag
-			for n in range(1,20):      
-				i = i + rowstep
-				j = j + colstep
-				if (i < 0):
-					i = i + 1	
-					colstep = colstep * -1
-					rowstep = rowstep * -1
-				if ( j < 0 ):
-					j = j + 1
-					colstep = colstep * -1
-					rowstep = rowstep * -1
-				zigzag[n] = abs(round(randBlockQ[i][j]))
-				if (randBlockQ[i][j] > 0):
-					sgn = '+'
-	 			else:
-					sgn = '-'
-			
-				if(zigzag[n] > 0):                                                #Checking for threshold
-					if( randBlockQ[i][j] > 0.5 and randBlockQ[i][j] < 0.7):
-						randBlockQ[i][j] = 0.7
-					if(randBlockQ[i][j] < -0.5 and randBlockQ[i][j] > -0.7):
-						randBlockQ[i][j] = -0.7
-				
-					if(bitCount < accbitstream.__len__()):           
-						if (accbitstream[bitCount] == '1'):                               #If bit to be encoded is 1
-							if (zigzag[n] % 2 == 0):                                    #Converting to odd if it is an even reconstruction point			
-								if sgn == '+':											#Incrementing to next step size to get odd							
-									randBlockQ[i][j] = (randBlockDCT[i][j] + newQM[i][j]) / newQM[i][j]							
-								else:
-									randBlockQ[i][j] = (randBlockDCT[i][j] - newQM[i][j]) / newQM[i][j]
-								   
-														
-						elif (accbitstream[bitCount] == '0'):								#If bit to be encoded is 0
-							if (zigzag[n] % 2 != 0):									#Converting to even if it is an odd reconstruction point
-								if sgn == '+':											#Incrementing to next step size to get even
-									randBlockQ[i][j] = (randBlockDCT[i][j] + newQM[i][j]) / newQM[i][j]
-								else:
-									randBlockQ[i][j] = (randBlockDCT[i][j] - newQM[i][j]) / newQM[i][j]   
-			
-						#print randBlockQ[i][j] , accbitstream[bitCount]
-					bitCount = bitCount + 1
-				elif (zigzag[n] == 0):
-					randBlockQ[i][j]=zigzag[n]
-				
-			irandBlockDCT = np.multiply(randBlockQ,newQM)
-			irandBlock = idct2(irandBlockDCT)
-			irandBlock = np.add(irandBlock,128)
-			newimg[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)] = np.uint8(np.round(irandBlock))
-
-	#img_component[0:256,0:256] = newimg
-	img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ] = newimg
+		randBlock = copy(img[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)])
+		randBlock = np.add(randBlock,-128)
+		randBlockDCT = dct2(randBlock)
+		randBlockQ = np.divide(randBlockDCT,newQM)
 	
-	if im.mode != 'L':
-		img_src[...,1] = img_component
-	else:
-		img_src = img_component
+		##Embedding
+		zigzag = np.zeros(20)
+		zigzag[0] = randBlockQ[0][0]
+		rowstep = -1		
+		colstep = 1
+		i = 0
+		j = 0
+		#Travelling zigzag
+		for n in range(1,20):      
+			i = i + rowstep
+			j = j + colstep
+			if (i < 0):
+				i = i + 1	
+				colstep = colstep * -1
+				rowstep = rowstep * -1
+			if ( j < 0 ):
+				j = j + 1
+				colstep = colstep * -1
+				rowstep = rowstep * -1
+			zigzag[n] = abs(round(randBlockQ[i][j]))
+			if (randBlockQ[i][j] > 0):
+				sgn = '+'
+ 			else:
+				sgn = '-'
+		
+			if(zigzag[n] > 0):                                                #Checking for threshold
+				if( randBlockQ[i][j] > 0.5 and randBlockQ[i][j] < 0.7):
+					randBlockQ[i][j] = 0.7
+				if(randBlockQ[i][j] < -0.5 and randBlockQ[i][j] > -0.7):
+					randBlockQ[i][j] = -0.7
+			
+				if(bitCount < accbitstream.__len__()):           
+					if (accbitstream[bitCount] == '1'):                               #If bit to be encoded is 1
+						if (zigzag[n] % 2 == 0):                                    #Converting to odd if it is an even reconstruction point			
+							if sgn == '+':											#Incrementing to next step size to get odd							
+								randBlockQ[i][j] = (randBlockDCT[i][j] + newQM[i][j]) / newQM[i][j]							
+							else:
+								randBlockQ[i][j] = (randBlockDCT[i][j] - newQM[i][j]) / newQM[i][j]
+							   
+													
+					elif (accbitstream[bitCount] == '0'):								#If bit to be encoded is 0
+						if (zigzag[n] % 2 != 0):									#Converting to even if it is an odd reconstruction point
+							if sgn == '+':											#Incrementing to next step size to get even
+								randBlockQ[i][j] = (randBlockDCT[i][j] + newQM[i][j]) / newQM[i][j]
+							else:
+								randBlockQ[i][j] = (randBlockDCT[i][j] - newQM[i][j]) / newQM[i][j]   
+		
+					#print randBlockQ[i][j] , accbitstream[bitCount]
+				bitCount = bitCount + 1
+			elif (zigzag[n] == 0):
+				randBlockQ[i][j]=zigzag[n]
+			
+		irandBlockDCT = np.multiply(randBlockQ,newQM)
+		irandBlock = idct2(irandBlockDCT)
+		irandBlock = np.add(irandBlock,128)
+		newimg[((I)*B+Sx):((I)*B+Sx+8), ((J)*B+Sy):((J)*B+Sy+8)] = np.uint8(np.round(irandBlock))
 
-	newim = scipy.misc.toimage(img_src,255,0,mode=im.mode)
-	destfile = filename
-	newim.save(destfile,'PNG', quality=100)
-	shellcommand = "convert '" + destfile + "' -quality 100 '" + destfile + "'"
-	os.system(shellcommand)
-	return accbitstream
+#img_component[0:256,0:256] = newimg
+img_component[ (img_component.shape[0]-256)/2 : ((img_component.shape[0]-256)/2)+256, (img_component.shape[1]-256)/2 : ((img_component.shape[1]-256)/2)+256 ] = newimg
+
+if im.mode != 'L':
+	img_src[...,1] = img_component
+else:
+	img_src = img_component
+
+newim = scipy.misc.toimage(img_src,255,0,mode=im.mode)
+#destfile = filename
+destfile = "newimage.jpg"
+newim.save(destfile,'PNG', quality=100)
+shellcommand = "convert '" + destfile + "' -quality 100 '" + destfile + "'"
+os.system(shellcommand)
+#return accbitstream
